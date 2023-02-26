b84866655ed9c06ac31c8cf1d93a8cc5553b5f0e
Johan Hake <johan.hake@gmail.com>
2023-02-26 20:46:59 +0100

    La til eksempler og løsningsforslag på oppgaver

---
+++ src/routes/eksempler_kap_6/6.3.2_lese_inn_data_fra_en_json-fil/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	// @ts-nocheck
+
+	const data = fetch('https://kode-1.github.io/person.json');
+	const person = data.json();
+
+</script>
+
+{#if person}
+	<p>Navn: {person.navn}</p>
+	<p>Alder: {person.alder}</p>
+{:else}
+	<p>Henter data...</p>
+{/if}
